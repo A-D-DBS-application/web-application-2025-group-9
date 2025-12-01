473bbed8a7c102e81073ac91859de905ddcf6af1
jonvinck <jonvinck.vinck@ugent.be>
2025-12-02 00:08:49 +0100

    partial update config

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config: 
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:password@localhost:port/dbname'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.rhhnxaxbtapjenunokzv:password@localhost:5432/dbname'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
