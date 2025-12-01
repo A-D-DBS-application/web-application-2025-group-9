396931c363f05600dd3322b50d73f7cff107e8ca
jonvinck <jonvinck.vinck@ugent.be>
2025-12-02 00:09:45 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config: 
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.rhhnxaxbtapjenunokzv:password@localhost:5432/dbname'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.rhhnxaxbtapjenunokzv:password@localhost:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
