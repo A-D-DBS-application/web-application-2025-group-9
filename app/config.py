8a237a496ccd61ca18c251b5741078e0e2edcf5d
jonvinck <jonvinck.vinck@ugent.be>
2025-12-08 14:10:44 +0100

    db

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 class Config: 
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.rhhnxaxbtapjenunokzv:password@localhost:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Group9!?@db.rhhnxaxbtapjenunokzv.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
